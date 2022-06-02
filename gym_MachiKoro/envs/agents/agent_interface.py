d5222cbf15143c828bfca1096e97313c3c5f2474
01001101011010010110010001100001 <anhdo@vis-laboratory.com>
2022-06-02 02:48:15 +0000

    NA học Monte Carlo

--- gym_MachiKoro/envs/agents/agent_interface.py
+++ gym_MachiKoro/envs/agents/agent_interface.py
@@ -1,9 +1,9 @@
-from gym_MachiKoro.envs.agents import agent_A as p1
+from gym_MachiKoro.envs.agents import agent_B as p1
 from gym_MachiKoro.envs.agents import agent_A as p2
 from gym_MachiKoro.envs.agents import agent_A as p3
 from gym_MachiKoro.envs.agents import agent_A as p4
 
-agent1 = p1.Agent('Đông')
+agent1 = p1.Agent('NA_RL')
 agent2 = p2.Agent('Tây')
 agent3 = p3.Agent('Nam')
 agent4 = p4.Agent('Bắc')
