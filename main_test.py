d554e64b6255221609e8f177f1d4b041ae2afd64
01001101011010010110010001100001 <anhdo@vis-laboratory.com>
2022-06-06 06:50:20 +0000

    Đang test kết quả batch 1

---
+++ main_test.py
@@ -0,0 +1,51 @@
+from copy import deepcopy
+import random
+import gym
+import gym_MachiKoro
+import time
+from contextlib import contextmanager
+import sys, os
+
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+env = gym.make('gym_MachiKoro-v0')
+
+def main():
+    env.reset()
+    env.run_game()
+
+    for i in range(500):
+        o,a,done,t = env.step(env.turn.action(deepcopy(env.dict_input)))
+        if done:
+            break
+
+    for p in env.players:
+        turn_id = env.players.index(p)
+        env.dict_input['Player'] = [p]
+        for i in range(1, env.players.__len__()):
+            env.dict_input['Player'].append(env.players[(turn_id+i) % env.players.__len__()])
+        
+        env.dict_input['Value_of_dice'] = 0
+
+        p.action(deepcopy(env.dict_input))
+
+    return env.p_name_victory
+
+
+count = {}
+for van in range(100):
+    with suppress_stdout():
+        name = main()
+    if name in count.keys():
+        count[name] += 1
+    else:
+        count[name] = 1
+    print(count)
